343614010eab72fe8d1e4740d0ff86f2cdc59866
Deborah <debbyuzuegbu@gmail.com>
2023-01-17 11:48:24 +0100

    working on home page

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -4,31 +4,35 @@ import { BsBookmark } from "react-icons/bs";
 import { AiOutlineHeart } from "react-icons/ai";
 import { CiAt } from "react-icons/ci";
 import "./Box.css";
+import Profile from '../Images/profile.png';
+import Post from '../Images/post.png';
 
 const Box = () => {
   return (
     <div className="container">
-      <div ClassName="section-1">
+      <div className="section-1">
         <div>
-          <img src="" alt="" />
+          <img src={Profile} alt="profile" />
         </div>
         <div>
           <div>FirstName Surname</div>
           <div>country</div>
         </div>
       </div>
-      <div>
+
+      <div className="section-2">
         <div>
           <FaHeartBroken /> <BsBookmark />
         </div>
         <div>
-          <img src="" alt="" />
+          <img className="post" src={Post} alt="post"/>
         </div>
         <div>
           <AiOutlineHeart />
           <CiAt />
         </div>
       </div>
+
       <div>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga similique
         illum eum fugit nostrum saepe facilis quaerat asperiores nisi ex,
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import Box from './Box'
+import React from 'react';
+import Box from './Box';
 
 const Home = () => {
   return (

e6237b390868d8a8200ac54540ab3d5eeffba1b9
thierno sadou diallo <thierved01@gmail.com>
2023-01-28 22:02:02 +0000

    added the info section

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -3,20 +3,37 @@ import React from "react";
 import "./Profile.css";
 
 export default function Profile() {
-    
- return (
+  return (
     <div className="wrapper">
-        <header>
-            <div className="btn">FEELZ</div>
-            <nav>
-                <a href="#"><img src="/public/images/icons/home.png" alt="" /></a>
-                <a href="#"><img src="/public/images/icons/newpost.png" alt="" /></a>
-            </nav>
-            <div className="profile"><img src="/public/images/icons/profile.jpg" alt="" /></div>
-        </header>
-        <main>
-
-        </main>
+      <header>
+        <div className="btn">FEELZ</div>
+        <nav>
+          <a href="#">
+            <img src="/public/images/icons/home.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/public/images/icons/newpost.png" alt="" />
+          </a>
+        </nav>
+        <div className="profile">
+          <img src="/public/images/icons/profile.jpg" alt="" />
+        </div>
+      </header>
+      <main>
+        <div className="cover">
+          <img src="/public/images/icons/profile.jpg" alt="" />
+        </div>
+        <div className="profile-info">
+          <h3>Evelyn Jonas</h3>
+          <p className="handle">@Angelevie</p>
+          <p className="city">Los Angeles</p>
+          <p className="about">
+            A n artist by day and a content creator by night. I love arts and
+            anything filled with nature
+          </p>
+          <button>Edit Profile</button>
+        </div>
+      </main>
     </div>
- );   
+  );
 }

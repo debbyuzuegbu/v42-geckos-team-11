fcb99bfd8c7b84d7daacd972f147672a9d7c2571
thierno sadou diallo <thierved01@gmail.com>
2023-01-30 20:18:03 +0000

    finished the static profile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -33,6 +33,30 @@ export default function Profile() {
           </p>
           <button>Edit Profile</button>
         </div>
+        <div className="detail">
+          <div className="profile-summery">
+            <div className="icon"></div>
+            <div className="summery">
+              <span>2</span><span>Posts</span>
+            </div>
+          </div>
+          <div className="profile-summery">
+            <div className="icon"></div>
+            <div className="summery">
+              <span>20</span><span>followers</span>
+            </div>
+          </div>
+          <div className="profile-summery">
+            <div className="icon"></div>
+            <div className="summery">
+              <span>50</span><span>following</span>
+            </div>
+          </div>
+        </div>
+        <div className="posts">
+          <img src="public/images/post1.png" alt="post1" />
+          <img src="public/images/post2.png" alt="post2" />
+        </div>
       </main>
     </div>
   );
